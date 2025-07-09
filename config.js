37d4b90c5af37ef0afeff2690e55b723603a3935
shirat-hanevel <shirathanevel@gmail.com>
2025-07-09 15:28:21 +0300

    Update config.js
    change to supabase client

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
-// מפתחות החיבור לסופהבייס - תמצא אותם בדשבורד של הפרויקט שלך תחת Settings -> API
-const SUPABASE_URL = 'https://hstsknresadmypumqrrc.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhzdHNrbnJlc2FkbXlwdW1xcnJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5OTQ0NTQsImV4cCI6MjA2NzU3MDQ1NH0.dlfmEIKpWJ9loc-LcuOeJESLlSgNIktbVF6Lm9DoRcQ';
-
 // יצירת חיבור לסופהבייס
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseClient = supabase.createClient(
+    'https://hstsknresadmypumqrrc.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhzdHNrbnJlc2FkbXlwdW1xcnJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5OTQ0NTQsImV4cCI6MjA2NzU3MDQ1NH0.dlfmEIKpWJ9loc-LcuOeJESLlSgNIktbVF6Lm9DoRcQ';
+);
+
+// ייצוא הלקוח כדי שיהיה זמין בקבצים אחרים
+window.supabaseClient = supabaseClient;
